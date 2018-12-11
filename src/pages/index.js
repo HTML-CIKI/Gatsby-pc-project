62d335a0faab7dbe57ab48e27e24a4605eafa99e
wss <13473992620@163.com>
2018-12-11 17:56:20 +0800

    hotsort

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Link } from 'gatsby'
 import Layout from '../components/Layout/layout'
 import Image from '../components/image'
 import './index.scss';
+import Hotsort from '../components/Hotsort/hotsort'
 const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
@@ -12,6 +13,7 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+    <Hotsort></Hotsort>
   </Layout>
 )
 
